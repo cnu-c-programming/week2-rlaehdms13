db8710ae179cd071570bc7afa6df2b6f2ed9b162
rlaehdms13 <de060103@gmail.com>
2026-03-16 21:36:14 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,27 @@
 
 int main()
 {
+    int num;
+    int isPrime = 1;
+
+    if(scanf("%d", &num) != 1) {
+        return 0;
+    }
+    if(num <= 1){
+        isPrime = 0;
+    } else{
+        for(int i = 2; i*i <= num; i++){
+            if(num % i == 0){
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+    if(isPrime){
+        printf("true\n");
+    }else {
+        printf("false\n");
+    }
 
     return 0;
 }
